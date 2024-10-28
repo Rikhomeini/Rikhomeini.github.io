f968aa68a5e1d338e90a78195d1d9d2c0dc2dcbb
Rikhomeini <rikogaul290@gmail.com>
2024-10-28 10:38:07 +0700

    menambahkan beberapa fitur di JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    // Dapatkan URL path halaman saat ini
+    const currentPath = window.location.pathname.toLowerCase(); 
+    navLinks.forEach(link => {
+        const linkPath = link.getAttribute('href').toLowerCase();
+
+        // Cek apakah path dari link sesuai dengan halaman saat ini
+        if (currentPath.includes(linkPath)) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+});
+
+
